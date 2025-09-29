af411b0f41e2416ef63bb79a5293eba53aabfbcf
[dyad] <pixelnegociosdigitais@gmail.com>
2025-09-29 19:08:50 -0300

    [dyad] Fixing TypeScript errors in server functions - wrote 2 file(s)

--- supabase/functions/create-user/index.ts
+++ supabase/functions/create-user/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0'
 
