9612f2f174b7c8a68c9ef9005bfd3353d813ad21
[dyad] <pixelnegociosdigitais@gmail.com>
2025-10-03 14:52:07 -0300

    [dyad] wrote 1 file(s)

---
+++ components/icons/QuestionIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const QuestionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9.247a.75.75 0 01.968-.043l2.5 2.25a.75.75 0 010 1.086l-2.5 2.25a.75.75 0 01-.968-.043L5.66 12.5l2.569-3.253zM15.772 9.247a.75.75 0 01-.968-.043l-2.5 2.25a.75.75 0 010 1.086l2.5 2.25a.75.75 0 01.968-.043L18.34 12.5l-2.569-3.253z" />
+    </svg>
+);
